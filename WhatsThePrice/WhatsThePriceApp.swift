6115877e3f26ebcb2d33d834101aff4cb500c733
Mannan Kant <mannankant2412@gmail.com>
2024-05-24 13:21:24 +0800

    Initial Commit

---
+++ WhatsThePrice/WhatsThePriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsThePriceApp.swift
+//  WhatsThePrice
+//
+//  Created by Jannat Bedi on 24/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsThePriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
